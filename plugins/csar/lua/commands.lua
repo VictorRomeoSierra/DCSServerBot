881a73d5a16bb8c5cd3397b8e26685475820463d
AI Dev Server <shifty@victorromeosierra.com>
2025-02-15 22:45:36 -0800

    try calling the function direct?

--- plugins/csar/lua/commands.lua
+++ plugins/csar/lua/commands.lua
@@ -9,13 +9,15 @@ local utils 	= base.require("DCSServerBotUtils")
 
 function dcsbot.csarUpdatePersistentData(json)
     log.write('DCSServerBot', log.DEBUG, 'CSAR: csarUpdatePersistentData() (commands.lua)')
-    local script = 'dcsbot._csarUpdatePersistentData(' .. utils.basicSerialize(json.data) .. ')'
+    local lua = net.lua2json(json.data)
+    local script = 'csar.setLives(' .. utils.basicSerialize(lua) .. ')'
     net.dostring_in('mission', 'a_do_script(' .. utils.basicSerialize(script) .. ')')
 end
 
 function dcsbot.csarSetLives(json)
     log.write('DCSServerBot', log.DEBUG, 'CSAR: csarSetLives() (commands.lua)')
-    local script = 'dcsbot._csarSetLives(' .. utils.basicSerialize(json.data) .. ')'
+    local lua = net.lua2json(json.data)
+    local script = 'csar.setLives(' .. utils.basicSerialize(lua) .. ')'
     net.dostring_in('mission', 'a_do_script(' .. utils.basicSerialize(script) .. ')')
 end
 
@@ -24,3 +26,5 @@ function dcsbot.csarBlockSlot(playerName, typeName, block)
     local script = 'dcsbot._blockSlot(' .. utils.basicSerialize(playerName) .. ',' ..  utils.basicSerialize(typeName) .. ',' .. utils.basicSerialize(block) .. ')'
     net.dostring_in('mission', 'a_do_script(' .. utils.basicSerialize(script) .. ')')
 end
+
+env.info("DCSServerBot - CSAR: commands.lua loaded.")
